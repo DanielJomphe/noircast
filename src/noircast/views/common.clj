f4be2dc4663dfe94d0abb1d3e0e4ed4ff0c44af6
Daniel Jomphe <danieljomphe@gmail.com>
2011-08-08 21:37:21 -0400

    initial structure generated by noir

---
+++ src/noircast/views/common.clj
@@ -0,0 +1,13 @@
+(ns noircast.views.common
+  (:use noir.core
+        hiccup.core
+        hiccup.page-helpers))
+
+(defpartial layout [& content]
+            (html5
+              [:head
+               [:title "noircast"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                content]]))
---
+++ src/noircast/views/welcome.clj
@@ -0,0 +1,10 @@
+(ns noircast.views.welcome
+  (:require [noircast.views.common :as common]
+            [noir.content.pages :as pages])
+  (:use noir.core
+        hiccup.core
+        hiccup.page-helpers))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to noircast"]))
