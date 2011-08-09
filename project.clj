c5731e61148f3b0a814d5a34b961a4b2ba737507
Daniel Jomphe <danieljomphe@gmail.com>
2011-08-08 21:40:43 -0400

    live on the edge

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject noircast "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.1.0"]]
+            :dependencies [[org.clojure/clojure "1.3.0-SNAPSHOT"]
+                           [noir "1.1.1-SNAPSHOT"]]
             :main noircast.server)
 
