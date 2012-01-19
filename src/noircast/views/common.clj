f991350f9848202dbe94906b40b133462a86be6b
Daniel Jomphe <danieljomphe@gmail.com>
2012-01-19 07:47:47 -0500

    Adapt layout for NoirCast applications

--- src/noircast/views/common.clj
+++ src/noircast/views/common.clj
@@ -3,12 +3,46 @@
         hiccup.core
         hiccup.page-helpers))
 
+(defpartial header-status []
+  [:section#status
+   [:div#debug]
+   [:div#wsMessages]
+   "STATUS"])
+
+(defpartial nav-menu [items]
+  [:nav [:ul (map
+              (fn [[url text]] [:li (link-to url text)])
+              items)]])
+
+(defpartial header-menu []
+  (nav-menu [["/"        "home"]
+             ]))
+
+(defpartial footer-menu []
+  (nav-menu []))
+
 (defpartial layout [& content]
   (html5
    [:head
+    ;; TODO make sure this is useful; configure
+    {:lang "fr"}
+    ;; TODO make sure server and ring middleware don't override this,
+    ;; especially on Windows. If present in HTTP headers, the value
+    ;; should be exactly 'Content-Type: text/html; charset="utf-8"'
+    ;; TODO evaluate Noir's wrap-utf8 middleware to deprecate this line
+    [:meta {:charset "utf-8"}]
     [:title "noircast"]
+    ;; TODO use Html5Boilerplate and/or Twitter's Bootstrap
     (include-css "/css/reset.css")
     (include-js "/cljs/bootstrap.js")]
    [:body
+    [:header [:h1 "HEADER"]
+     (header-status)
+     (header-menu)
+     [:hr]]
     [:div#wrapper
-     content]]))
+     content]
+    [:footer
+     [:hr]
+     (footer-menu)
+     [:h1 "FOOTER"]]]))
--- src/noircast/views/welcome.clj
+++ src/noircast/views/welcome.clj
@@ -5,10 +5,4 @@
         hiccup.page-helpers))
 
 (defpage "/" []
-  (common/layout
-   (link-to "/welcome" "Welcome")))
-
-(defpage "/welcome" []
-  (common/layout
-   [:h1 "Welcome to NoirCast!"]
-   [:p  "Hope you like it."]))
+  (common/layout))
