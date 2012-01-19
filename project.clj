e56c2bc61324200dea7a076e3ce1e8b562278820
Daniel Jomphe <danieljomphe@gmail.com>
2012-01-19 06:51:33 -0500

    Update dependencies
    Clojure 1.3 and Noir 1.2.2.
    Debug utilities.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject noircast "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
-  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
-                 [noir "1.1.1-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [noir "1.2.2"]
+                 [clj-stacktrace "0.2.4"] ;fixes a transitive vn conflict
+                 ]
+  :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :main noircast.server)
